268b6f296bf58733a206429de7aa0658df1e0077
shreegowri <gaganandh@gmail.com>
2022-07-05 11:32:31 +0530

    [shreegowri]check weather the file exist or not

---
+++ FileIO/FileIO/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+namespace FileIO
+
+{
+    class Program
+    {
+        public static void FileExist()
+        {
+            string InputFile = @"G:\visualstudio\FileIOTextFile\FileIO.txt";
+            if(File.Exists(InputFile))
+            {
+                Console.WriteLine("File Exist");
+            }
+            else
+            {
+                Console.WriteLine("not Exist");
+            }
+        }
+        
+        public static void Main(string[]args)
+        {
+
+
+            FileExist();
+            
+        }
+    }
+}
