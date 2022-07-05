3d4f1c7b7a923397aff13080fe7dfb9d55063d74
shreegowri HD <53620679+gaganakodre@users.noreply.github.com>
2022-07-05 12:04:24 +0530

    Update Program.cs

--- FileIO/FileIO/Program.cs
+++ FileIO/FileIO/Program.cs
@@ -17,12 +17,48 @@ namespace FileIO
                 Console.WriteLine("not Exist");
             }
         }
-        
+        public static void ReadFile()
+        {
+            string InputFile = @"G:\visualstudio\FileIOTextFile\FileIO.txt";
+            using (StreamReader read = new StreamReader(InputFile))
+            {
+                string s = "";
+                while ((s = read.ReadLine()) != null)
+                {
+                    Console.WriteLine(s);
+                }
+                read.Close();
+            }
+        }
+        public static void CopyFile()
+        {
+            string path = @"G:\visualstudio\FileIOTextFile\FileIO.txt";
+            string CopyPath = @"G:\visualstudio\FileIOTextFile\FileIO1.txt";
+            File.Copy(path, CopyPath);
+        }
+        public static void DeleteFile()
+        {
+            string Path = @"G:\visualstudio\FileIOTextFile\FileIO1.txt";
+            File.Delete(Path);
+            Console.WriteLine("File deleted sucessfully");
+        }
+        public static void ReadAllText()
+        {
+            String path = @"G:\visualstudio\FileIOTextFile\FileIO.txt";
+            string lines;
+            lines = File.ReadAllText(path);
+            Console.WriteLine(lines);
+            Console.ReadKey();
+        }
         public static void Main(string[]args)
         {
 
 
             FileExist();
+            ReadFile();
+            CopyFile();
+            DeleteFile();
+            ReadAllText();
             
         }
     }
